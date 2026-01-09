4692e040fa7770e2099bd8261f5e1fdeebac3185
mikereiddigital <mike.reid@justice.gov.uk>
2026-01-09 11:56:36 +0000

    resolved issue

--- test/unit-test/main.tf
+++ test/unit-test/main.tf
@@ -1,7 +1,16 @@
-# Mock domain validation options for testing
+# Create certificate for testing
+resource "aws_acm_certificate" "test_certificate" {
+  domain_name               = "platforms-test.modernisation-platform.service.justice.gov.uk"
+  validation_method         = "DNS"
+  subject_alternative_names = ["*.testing.platforms-test.modernisation-platform.service.justice.gov.uk"]
+  lifecycle {
+    create_before_destroy = true
+  }
+  tags = local.tags
+}
+
+# Mock domain validation options for testing (to avoid "known after apply" issue)
 locals {
-  mock_certificate_arn = "arn:aws:acm:eu-west-2:123456789012:certificate/12345678-1234-1234-1234-123456789012"
-  
   mock_domain_validation_options = toset([
     {
       domain_name           = "platforms-test.modernisation-platform.service.justice.gov.uk"
@@ -21,7 +30,7 @@ locals {
 module "module_test" {
   source = "../../"
 
-  certificate_arn                          = local.mock_certificate_arn
+  certificate_arn                          = aws_acm_certificate.test_certificate.arn
   domain_validation_options                = local.mock_domain_validation_options
   is-production                            = local.is-production
   zone_id_core_vpc_public                  = data.aws_route53_zone.external.zone_id
