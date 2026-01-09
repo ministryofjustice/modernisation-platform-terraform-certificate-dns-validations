bfa3e1a9eef6ceb1d3cabc99b2372aab355f30fd
mikereiddigital <mike.reid@justice.gov.uk>
2026-01-09 12:08:02 +0000

    Updated assertions

--- test/module_test.go
+++ test/module_test.go
@@ -19,15 +19,12 @@ func TestModule(t *testing.T) {
 	terraform.InitAndApply(t, terraformOptions)
 
 	// Assert outputs
-	certificateValidationID := terraform.Output(t, terraformOptions, "certificate_validation_id")
-	assert.NotEmpty(t, certificateValidationID, "certificate_validation_id should not be empty")
-
 	validationRecordFQDNs := terraform.OutputList(t, terraformOptions, "validation_record_fqdns")
 	assert.NotEmpty(t, validationRecordFQDNs, "validation_record_fqdns should not be empty")
-	assert.Greater(t, len(validationRecordFQDNs), 0, "Should have at least one validation record FQDN")
+	assert.Equal(t, 2, len(validationRecordFQDNs), "Should have exactly 2 validation record FQDNs")
 
 	// Verify validation records contain expected domain
 	for _, fqdn := range validationRecordFQDNs {
-		assert.Contains(t, fqdn, "modernisation-platform.service.justice.gov.uk", "FQDN should contain expected domain")
+		assert.Contains(t, fqdn, "platforms-test.modernisation-platform.service.justice.gov.uk", "FQDN should contain expected domain")
 	}
 }
