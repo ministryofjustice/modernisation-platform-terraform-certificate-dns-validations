bc143eb6378af5f2f78ffa688dc013b59eb10d38
mikereiddigital <mike.reid@justice.gov.uk>
2026-01-09 11:16:58 +0000

    testing

--- test/module_test.go
+++ test/module_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/gruntwork-io/terratest/modules/terraform"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestModule(t *testing.T) {
@@ -17,4 +18,16 @@ func TestModule(t *testing.T) {
 
 	terraform.InitAndApply(t, terraformOptions)
 
+	// Assert outputs
+	certificateValidationID := terraform.Output(t, terraformOptions, "certificate_validation_id")
+	assert.NotEmpty(t, certificateValidationID, "certificate_validation_id should not be empty")
+
+	validationRecordFQDNs := terraform.OutputList(t, terraformOptions, "validation_record_fqdns")
+	assert.NotEmpty(t, validationRecordFQDNs, "validation_record_fqdns should not be empty")
+	assert.Greater(t, len(validationRecordFQDNs), 0, "Should have at least one validation record FQDN")
+
+	// Verify validation records contain expected domain
+	for _, fqdn := range validationRecordFQDNs {
+		assert.Contains(t, fqdn, "modernisation-platform.service.justice.gov.uk", "FQDN should contain expected domain")
+	}
 }
